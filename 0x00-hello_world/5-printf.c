1dcab377bfba37397287515ad6dfdc5eb8bfdc8f
MoHamdy94 <mohamedhamdy994@gmail.com>
2023-08-10 12:48:11 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main is the ENtry point
+ *
+ * return 0 is succes
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return(0);
