306cf4f64d4b8e174ed45810856667a079c64876
MoHamdy94 <mohamedhamdy994@gmail.com>
2023-08-11 12:12:38 -0700

    commit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,11 +1,9 @@
 #include<stdio.h>
 /**
-* main - Entry point
-Description: print all possible different
-* combinations of two digits.
-*
-* Return: Always 0 (Success)
-*/
+ * main - prints all possible different combinations of two digits
+ * Return: ALways 0 (Success)
+ */
+
 int main(void)
 {
 int digit1 = 0, digit2;
