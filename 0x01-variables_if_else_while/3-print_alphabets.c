32c52b6e0954e6734de50f52dafbc818155ea037
MoHamdy94 <mohamedhamdy994@gmail.com>
2023-08-11 10:25:29 -0700

    commit

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Prints the alphabet.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char ch = 'a';
+char cH = 'A';
+while (ch <= 'z')
+{
+putchar(ch);
+ch++;
+}
+while (cH <= 'Z');
+cH++
+}
+putchar(\n);
+return (0);
+}
