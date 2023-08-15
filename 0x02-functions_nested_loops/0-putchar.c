c40ad6e832d9c01bb91328f5cc855a00a02743ed
MoHamdy94 <mohamedhamdy994@gmail.com>
2023-08-15 07:56:21 -0700

    commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - print _putchar
+ * Return: 0
+ */
+int main(void)
+{
+char ptc[] = "_putchar";
+int ch;
+for (ch = 0; ch < 8 ; ch++)
+{
+_putchar(ptc[ch]);
+}
+_putchar('\n');
+return (0);
