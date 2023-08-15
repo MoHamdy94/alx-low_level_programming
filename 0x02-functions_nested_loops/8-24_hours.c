a19593d75eee899f631f405a5e65e2f2ecc9ac9b
MoHamdy94 <mohamedhamdy994@gmail.com>
2023-08-15 11:39:52 -0700

    commit

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -5,7 +5,7 @@
 */
 void jack_bauer(void)
 {
-int min, hr:
+int min, hr;
 for (hr = 0; hr < 23; hr++)
 for (min = 0; min < 59; min++)
 {
@@ -13,5 +13,5 @@ putchar((hr / 10) + 48);
 putchar((hr % 10) + 48);
 putchar(':');
 putchar((min / 10) + 48);
-putchar((min % 10) + 48):
+putchar((min % 10) + 48);
 putchar("\n");
