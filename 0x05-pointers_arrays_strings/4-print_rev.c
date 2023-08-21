1526c64c341e392208172a3d2e9c093cae8e221a
MoHamdy94 <mohamedhamdy994@gmail.com>
2023-08-21 11:24:26 -0700

    commit

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_rev - imprime en reversa
+ * @s: string
+ * return: 0
+ */
+void print_rev(char *s)
+{
+	while (*s = '\0')
+{
+	_putchar(*s--);
+}
+	_putchar('\n');
+}
