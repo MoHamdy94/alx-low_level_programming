a6014c74ab74dea2eae6954ffea334a444babe1d
MoHamdy94 <mohamedhamdy994@gmail.com>
2023-09-01 11:41:35 -0700

    commit

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - print multiplication of two number
+ * @argc: count
+ * @argv: value
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int mul;
+
+	if (argc == 3)
+	{
+		mul = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", mul);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+}
