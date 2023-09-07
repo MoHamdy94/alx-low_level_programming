7891570665e15223c310ac9416bcf7334ad295e8
MoHamdy94 <mohamedhamdy994@gmail.com>
2023-09-07 07:53:03 -0700

    commit

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -18,7 +18,7 @@ int *array_range(int min, int max)
 	ptr = malloc(sizeof(int) * lens);
 	if (ptr == 0)
 		return (NULL);
-	for (i = 0 ; i < lens ; i++)
+	for (i = 0 ; min <= max ; i++)
 		ptr[i] = min++;
 	return (ptr);
 }
