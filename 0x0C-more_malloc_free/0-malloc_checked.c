62d7eaf08b03ef612f0dd8d13f7b28f3a641e2da
MoHamdy94 <mohamedhamdy994@gmail.com>
2023-09-07 06:35:41 -0700

    commit

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+
+/**
+ * *malloc_checked - allocate memory using malloc and exit if failed
+ * @b: int
+ * Return: 98
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	int *m = malloc(b);
+
+	if (m == 0)
+		exit(98);
+
+	return (m);
+}
